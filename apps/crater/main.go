f22ca22ea05c3d7836b6cd80970938fa481eb2ca
geller99 <gellera800@gmail.com>
2023-07-14 19:36:22 -0600

    FIX: updated env loader

--- apps/crater/main.go
+++ apps/crater/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"github.com/joho/godotenv"
 	"net/http"
 	"os"
 	"tailzprotocol/routes"
@@ -11,16 +12,27 @@ func home(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("Wagging....Tailz"))
 }
 
+func loadEnv () {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+
+	loadEnv()
+
 	mux := http.NewServeMux()
 
 	// routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/api/auth", routes.AuthHandler)
 
+	
 
 	PORT, exists := os.LookupEnv("PORT")
-	
+
 	if exists {
 		log.Printf("Starting server on port %s \n", PORT)
 		err := http.ListenAndServe(":"+PORT, mux)
